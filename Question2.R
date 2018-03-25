4735c7b8d94409dd7e6ba5df2fe9ea248e7f61ae
RyanHiroshi <35421157+RyanHiroshi@users.noreply.github.com>
2018-03-25 22:39:38 +0700

    Create Question2.R

---
+++ Question2.R
@@ -0,0 +1,27 @@
+#Product A = x
+#Product B = y
+
+#Persamaan
+#-0.2x + 0.8y <= 0
+#x            <= 100
+#2x    + 4y   <= 240
+#Z = 20x + 50y
+
+library(lpSolveAPI)
+
+Q2 = make.lp(3,2)
+
+set.column(Q2, 1, c(-0.2, 1, 2))
+set.column(Q2, 2, c(0.8,0,4))
+
+set.constr.type(Q2, c("<=","<=","<="))
+set.rhs(Q2, c(0,100,240))
+
+set.objfn(Q2, c(20,50))
+lp.control(Q2, sense='max')
+solve(Q2)
+
+get.objective(Q2)
+get.variables(Q2)
+
+#plot(Q2)
